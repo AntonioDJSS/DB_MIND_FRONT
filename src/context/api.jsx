40d1aa84a334a4c4e42e40db983807f379027c4c
Jesus <antoniodjss2000@gmail.com>
2023-09-26 17:39:35 -0600

    Actualizado

--- src/context/api.jsx
+++ src/context/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/tramiteController/mostrarTramitesTodo";
+const API_URL = "https://dbwebmind.onrender.com/api/tramiteController/mostrarTramitesTodo";
 
 export const obtenerTramites = async () => {
   try {
